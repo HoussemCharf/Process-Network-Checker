755271b321ac0388a0e07ccd00406a91bdcb9b89
Houssem Charfeddine <houssem.charf@gmail.com>
2016-04-28 23:39:56 +0200

    Added files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(381, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Find Process"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(381, 238)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Kill Selected"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(381, 288)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(24, 48)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(327, 128)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Process Currently Using Network:"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(381, 102)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Credit"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 185)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(188, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Choose Process you wanna terminate:"
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(27, 201)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(324, 124)
+        Me.CheckedListBox1.TabIndex = 8
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(27, 340)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(70, 17)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "Select All"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(546, 369)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Network Process Checker v1.2 - By Houssem Charfeddine"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,178 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim l As New List(Of Integer)()
+    Dim pl As New List(Of String)()
+    Private Sub doo()
+        Dim proc As ProcessStartInfo = New ProcessStartInfo("cmd.exe")
+        Dim pr As Process
+        Dim ch As String
+
+
+
+        proc.CreateNoWindow = True
+        proc.Verb = "runas"
+        proc.UseShellExecute = False
+        proc.RedirectStandardInput = True
+        proc.RedirectStandardOutput = True
+        pr = Process.Start(proc)
+        pr.StandardInput.WriteLine("cls")
+        pr.StandardInput.WriteLine("netstat  -n -o & pause")
+        pr.StandardInput.Close()
+
+        Dim r As StreamReader = pr.StandardOutput
+        Dim line As String
+        line = r.ReadLine()
+        TextBox1.Text = ""
+
+
+        Do
+
+            If line.Contains("ESTABLISHED") Then
+                ch = line.Substring(line.IndexOf("ESTABLISHED"), line.Length - line.IndexOf("ESTABLISHED"))
+                ch = ch.Trim()
+                ch = ch.Remove(0, 16)
+
+                If Not (l.Contains(CInt(ch))) Then
+
+                    If CInt(ch) > 1000 Then
+                        l.Add(CInt(ch))
+                    End If
+                End If
+
+            End If
+            line = r.ReadLine()
+        Loop Until line.Contains("Press") Or line.Contains("touche")
+
+        pr.StandardOutput.Close()
+        Dim pr1 As Process = Process.Start(proc)
+        pr1.StandardInput.WriteLine("cls")
+        pr1.StandardInput.WriteLine("tasklist & pause")
+        pr1.StandardInput.Close()
+        Dim b As StreamReader = pr1.StandardOutput
+        line = b.ReadLine()
+
+        Do
+
+            If Test(l, line) Then
+                pl.Add(line.Remove(line.IndexOf(".")))
+                TextBox1.Text = line.Remove(line.IndexOf(".")) & "  PID=" & CStr(l.ElementAt(Pid(l, line))) & vbCrLf & TextBox1.Text
+
+            End If
+
+            line = b.ReadLine()
+        Loop Until line.Contains("Press") Or line.Contains("touche")
+        checkme(pl, CheckedListBox1)
+        pr1.StandardOutput.Close()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        doo()
+
+
+    End Sub
+
+    Private Function Test(ByVal L As List(Of Integer), ByVal ch As String) As Boolean
+        Dim R As Boolean = False
+        For Each c As Integer In L
+            If ch.Contains(CStr(c)) Then
+                R = True
+            End If
+        Next
+        Return R
+    End Function
+    Private Function Pid(ByVal L As List(Of Integer), ByVal ch As String) As Integer
+
+        Dim pos As Integer = 0
+
+        For Each c In L
+            If ch.Contains(CStr(c)) Then
+                pos = L.IndexOf(c)
+
+            End If
+        Next
+        Return pos
+
+    End Function
+    Private Sub checkme(ByVal l As List(Of String), ByRef c As CheckedListBox)
+        c.Items.Clear()
+
+
+        Dim ch As String
+        For Each ch In l
+            c.Items.Add(ch)
+
+        Next
+        l.Clear()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim proc As ProcessStartInfo = New ProcessStartInfo("cmd.exe")
+        Dim pr As Process
+        Dim t As New List(Of Integer)()
+        Dim mee As String
+
+        If TextBox1.Text = "" Then
+            MessageBox.Show("find them before you kill them :P")
+
+
+
+        Else
+
+            proc.CreateNoWindow = True
+            proc.Verb = "runas"
+            proc.UseShellExecute = False
+            proc.RedirectStandardInput = True
+            proc.RedirectStandardOutput = True
+            pr = Process.Start(proc)
+
+            For Each item In CheckedListBox1.CheckedItems
+                For Each mee In TextBox1.Text.Split(vbNewLine)
+                    If mee.Contains(item.ToString) Then
+                        t.Add(CInt(mee.Substring(mee.IndexOf("=") + 1)))
+                    End If
+                Next
+            Next
+
+
+            For Each c As Integer In t
+                pr.StandardInput.WriteLine("Taskkill /PID " & CStr(c))
+            Next
+
+            pr.StandardInput.Close()
+            doo()
+        End If
+    End Sub
+
+
+
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MessageBox.Show("                          All Right Reserved © 2016 - Houssem Charfeddine" & vbCrLf & "this app was created by Houssem Charfeddine in order to help people check their use of internet and to keep track on active process using it in the background all you need to do is press a button and voila")
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Dim i As Integer
+        If Not (TextBox1.Text = "") Then
+            If CheckBox1.Checked Then
+                For i = 0 To CheckedListBox1.Items.Count() - 1
+
+                    CheckedListBox1.SetItemChecked(i, True)
+                Next
+            Else
+
+                For i = 0 To CheckedListBox1.Items.Count() - 1
+
+                    CheckedListBox1.SetItemChecked(i, False)
+                Next
+
+            End If
+        End If
+    End Sub
+End Class
